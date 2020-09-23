0399f8f19c6a24d6df8769072679a48c77e46418
Ash Midgley <ayelgdom@tutanota.com>
2020-09-23 03:41:04 +0100

    Finish off 1.4

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -13,30 +13,43 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+    locations := make(map[string][]string)
+
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
-		}
-	}
+        fmt.Println("Please include filenames as arguments.")
+        return
+    }
+
+    for _, file := range files {
+        f, err := os.Open(file)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+            continue
+        }
+        input := bufio.NewScanner(f)
+        for input.Scan() {
+            text := input.Text()
+            counts[text]++
+            if !contains(locations[text], file) {
+                locations[text] = append(locations[text], file)
+            }
+        }
+        f.Close()
+    }
+
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%v\n", n, line, locations[line])
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
+func contains(arr []string, file string) bool {
+    for _, value := range arr {
+        if value == file {
+            return true
+        }
+    }
+    return false
 }
